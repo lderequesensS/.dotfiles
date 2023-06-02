e66ab19298a3f71885dfe8a056409b403f6f4be3
Leonardo de Requeséns <leonardo.sobarzo@protonmail.com>
2023-06-02 09:19:40 -0400

    Adding lsp-zero
    This is a "testing" commit, I want to see if this pseudo minimal
    configurations is better/faster than the one on kickstart.nvim if not
    then I will switch back

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -125,6 +125,27 @@ require("lazy").setup({
   -- Undotree
   {
     'mbbill/undotree',
-  }
+  },
 
+  -- LSP
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  },
 }, {})
