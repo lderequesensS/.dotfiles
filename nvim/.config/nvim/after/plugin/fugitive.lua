768d49635c2c48932706d53814ff142762d9d04b
Leonardo de Requeséns <leonardo.sobarzo@protonmail.com>
2023-06-12 23:15:17 -0400

    Add keymap to merge conflicts on fugitive

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- For conflict merge on dv view
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
