b8eb2fbd0ae372433e0d426e4762fc2d7b29db76
lderequesens <leonardo.sobarzo@protonmail.com>
2025-03-04 03:08:32 -0300

    Configure Sway
    Kind of nice but still testing

---
+++ sway/.config/sway/grim.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S bash -e
+# Source: https://github.com/rhoot/dotconfig/blob/master/configs/sway/grim.sh
+
+case "$1" in
+
+	"" | "all")
+		grim - | swappy -f -
+		;;
+
+	"screen")
+		output="$(swaymsg --type get_outputs | jq --raw-output 'map(select(.focused == true)) | first.name')"
+		grim -o "$output" - | swappy -f -
+		;;
+
+	"window")
+		region="$(swaymsg -t get_tree | jq --raw-output 'recurse(.nodes[]) | select(.focused == true).rect | "\(.x),\(.y) \(.width)x\(.height)"')"
+		grim -g "$region" - | swappy -f -
+		;;
+
+	"selection")
+		grim -g "$(slurp)" - | swappy -f -
+		;;
+
+	"*")
+		printf "unknown mode" > /dev/stderr
+		;;
+
+esac
